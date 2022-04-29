d312315a7f16d34bf5d9fa6a2ba6a66b175c6713
Vladislav Kukharev <v.kukharev98@gmail.com>
2022-04-30 03:05:26 +0400

    init

---
+++ components/playerLayout.tsx
@@ -0,0 +1,9 @@
+export const PlayerLayout = ({ children }) => {
+  return (
+    <div className="h-screen w-screen">
+      <div>Layout</div>
+      <div>{children}</div>
+      <div>Player</div>
+    </div>
+  );
+};
---
+++ pages/_app.tsx
@@ -0,0 +1,13 @@
+import "reset-css";
+import "../styles/globals.css";
+import { PlayerLayout } from "../components/playerLayout";
+
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <PlayerLayout>
+      <Component {...pageProps} />;
+    </PlayerLayout>
+  );
+};
+
+export default MyApp;
