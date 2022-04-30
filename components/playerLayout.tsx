74fe763aab318c430ed44c00e8c4e002c2cf3158
Vladyslav Kukharev <v.kukharev98@gmail.com>
2022-04-30 23:19:29 +0400

    feat: add sidebar (#1)

--- components/playerLayout.tsx
+++ components/playerLayout.tsx
@@ -1,9 +1,17 @@
+import Sidebar from "./sidebar";
+
 export const PlayerLayout = ({ children }) => {
   return (
     <div className="h-screen w-screen">
-      <div>Layout</div>
-      <div>{children}</div>
-      <div>Player</div>
+      <div className="bg-black absolute top-0 bottom-0 left-0 w-[250px] h-[calc(100vh-90px)]">
+        <Sidebar />
+      </div>
+      <div className="bg-slate-500 justify-center ml-[250px] mb-[90px]">
+        {children}
+      </div>
+      <div className="bg-gray-900 w-screen absolute z-1 left-0 bottom-0 h-[90px]">
+        Player
+      </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,26 @@
 module.exports = {
+  mode: "jit",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          100: "#F5F5F5",
+          200: "#EEEEEE",
+          300: "#E0E0E0",
+          400: "#BDBDBD",
+          500: "#9E9E9E",
+          600: "#757575",
+          700: "#616161",
+          800: "#424242",
+          900: "#181818",
+          1000: "#212121",
+        },
+      },
+    },
   },
   plugins: [],
 };
