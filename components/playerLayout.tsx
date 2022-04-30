ef652f8ab20d2ce2c25cb3ee1fe5cb9ced2def18
Vladyslav Kukharev <v.kukharev98@gmail.com>
2022-05-01 02:39:11 +0400

    feat: add prisma schemas (#2)

--- components/playerLayout.tsx
+++ components/playerLayout.tsx
@@ -6,9 +6,7 @@ export const PlayerLayout = ({ children }) => {
       <div className="bg-black absolute top-0 bottom-0 left-0 w-[250px] h-[calc(100vh-90px)]">
         <Sidebar />
       </div>
-      <div className="bg-slate-500 justify-center ml-[250px] mb-[90px]">
-        {children}
-      </div>
+      <div className="bg-slate-500 justify-center ml-[250px]">{children}</div>
       <div className="bg-gray-900 w-screen absolute z-1 left-0 bottom-0 h-[90px]">
         Player
       </div>
